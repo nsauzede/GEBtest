dbcc428e0361d0b9c77a0e2bbfd51510dd5ff6cd
Nicolas Sauzede <nsauzede@laposte.net>
2021-02-02 00:57:57 +0100

    Use C.printf the same way for every languages, to be fair

--- Qseq/qseq.v
+++ Qseq/qseq.v
@@ -1,9 +1,17 @@
-//n:=2000000
-n:=40000
+n:=20000000
+//n:=40000
 //n:=20
 mut q:=[]int{}
 q<<1 q << 1
 for i:=2; i < n; i++ {
     q<<q[i - q[i-1]] + q[i - q[i-2]]
 }
-println('Q=${q}')
+//println('Q=${q}')
+C.printf("Q=[")
+for i:=0; i<q.len; i++ {
+        if i>0 {
+                C.printf(", ")
+        }
+        C.printf("%d", q[i])
+}
+C.printf("]\n")
